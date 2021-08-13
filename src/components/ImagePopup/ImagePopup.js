a7f3c0f18a77df2e55d3ba6f1b0436b8a3b116ea
EvgenII L <genja1988@inbox.ru>
2021-08-13 16:07:50 +0300

    feat: some feat

--- src/components/ImagePopup/ImagePopup.js
+++ src/components/ImagePopup/ImagePopup.js
@@ -2,12 +2,21 @@ import './ImagePopup.css';
 
 function ImagePopup(props) {
 
+    function handleClick(e) {
+        e.preventDefault();
+        if (e.target.classList.contains('overlay')) {
+            props.onClose();
+            console.log(e.target.scrollY());
+            e.target.focus();
+        }
+
+    }
     return (
         !!props.photo ?
-            <div className={'overlay overlay_visible'}>
-                <div className="popup popup_purpose_view-photo">
+            <div className={'overlay overlay_visible'} onClick={handleClick}>
+                <div className="popup">
                     <img className="popup__image" src={props.photo.link} alt={"фото " + props.photo.name} />
-                    <h2 className="popup__title popup__title_purpose_view-photo">{props.photo.name}</h2>
+                    <h2 className="popup__title">{props.photo.name}</h2>
                     <button aria-label="закрыть" type="button" className="popup__button-close" onClick={props.onClose}></button>
                 </div>
             </div>
